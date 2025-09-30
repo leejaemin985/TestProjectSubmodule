f760ab9658dbfc2750694c15e59fc77e48716505
DESKTOP-29AOU10\USER <leejaemin@illuni.com>
2025-09-30 16:13:20 +0900

    Common SoundObject 변경

--- Runtime/CommonSoundObject.cs
+++ Runtime/CommonSoundObject.cs
@@ -4,38 +4,38 @@ using UnityEngine.Audio;
 
 namespace Utility.Sound
 {
-	[RequireComponent(typeof(AudioSource))]
-	public abstract class CommonSoundObject<TEnum> : MonoBehaviour where TEnum : Enum
-	{
-		private bool initialized = false;
-		private AudioSource audioSource = default;
+    [RequireComponent(typeof(AudioSource))]
+    public abstract class CommonSoundObject<TEnum> : MonoBehaviour where TEnum : Enum
+    {
+        private bool initialized = false;
+        private AudioSource audioSource = default;
 
-		public AudioSource Audio
-		{
-			get
-			{
-				if (initialized == false) throw new InvalidOperationException("SoundObject is not initialized.");
-				if (audioSource==null)
-				{
-					audioSource = GetComponent<AudioSource>();
-				}
-				return audioSource;
-			}
-		}
+        public AudioSource Audio
+        {
+            get
+            {
+                if (initialized == false) throw new InvalidOperationException("SoundObject is not initialized.");
+                if (audioSource == null)
+                {
+                    audioSource = GetComponent<AudioSource>();
+                }
+                return audioSource;
+            }
+        }
 
-		public abstract TEnum SoundType { get; }
+        public abstract TEnum SoundType { get; }
 
-		public virtual void Initialize(AudioMixerGroup outputGroup)
-		{
-			if (outputGroup == null) return;
+        public virtual void Initialize(AudioMixerGroup outputGroup)
+        {
+            if (outputGroup == null) return;
 
-			initialized = true;
-			Audio.outputAudioMixerGroup = outputGroup;
-		}
+            initialized = true;
+            Audio.outputAudioMixerGroup = outputGroup;
+        }
 
-		public void PlayOneShot(AudioClip audioClip)
-		{
-			Audio.PlayOneShot(audioClip);
-		}
-	}
+        public void PlayOneShot(AudioClip audioClip)
+        {
+            Audio.PlayOneShot(audioClip);
+        }
+    }
 }
--- Runtime/GameAudioMixerController.cs
+++ Runtime/GameAudioMixerController.cs
@@ -4,10 +4,23 @@ namespace Utility.Sound
 
     public class GameAudioMixerController : CommonAudioMixerController<SoundType>
     {
-        protected override string MIXER_PATH => throw new System.NotImplementedException();
+        protected override string MIXER_PATH => "AudioMixer/GameAudioMixer";
+        protected override string GROUP_BASE => "Group_{0}";
+        protected override string VOLUME_BASE => "Volume_{0}";
 
-        protected override string GROUP_BASE => throw new System.NotImplementedException();
 
-        protected override string VOLUME_BASE => throw new System.NotImplementedException();
+        private static GameAudioMixerController instance;
+        public static GameAudioMixerController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new();
+                    instance.Initialize();
+                }
+                return instance;
+            }
+        }
     }
 }
