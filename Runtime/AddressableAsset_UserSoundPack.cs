a6a13dfdcfcae60d6c9e6f6a1b88342ba5986e2e
DESKTOP-29AOU10\USER <leejaemin@illuni.com>
2025-10-01 16:18:26 +0900

    유저 사운드팩 추가

---
+++ Runtime/AddressableAsset_UserSoundPack.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Addressable
+{
+    public class AddressableAsset_UserSoundPack : MonoBehaviour
+    {
+        [Header("FSM State")]
+        [SerializeField] private AudioClip roarStateSE;
+    }
+}
